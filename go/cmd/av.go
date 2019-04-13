ac8af6c2e2b1b0a5eefb66bff762d76fbad6dc20
Jordan Sissel <jls@semicomplete.com>
2019-04-12 23:39:22 -0700

    Print status if no arguments given

--- go/cmd/av.go
+++ go/cmd/av.go
@@ -12,7 +12,7 @@ var avCmd = &cobra.Command{
 	Use:   "av",
 	Run:   avRun,
 	Short: "Control Denon A/V Reciever",
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.ArbitraryArgs,
 }
 
 func init() {
@@ -21,6 +21,19 @@ func init() {
 
 func avRun(cmd *cobra.Command, args []string) {
 	avr := denon.Discover()
+
+	if len(args) == 0 {
+		status, err := avr.Query()
+		if err != nil {
+			logger.Errorw(err.Error())
+			os.Exit(1)
+		}
+
+		logger.Infof("Power: %t", status.Power)
+		logger.Infof("Volume: %.1f", status.Volume)
+		logger.Infof("Input: %s", status.Input.Name)
+		return
+	}
 
 	for i, command := range os.Args[1:] {
 		avr.Command(command)
